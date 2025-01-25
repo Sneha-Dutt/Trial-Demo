cb8ca90b046de4f48707f8d8fca7a825eb3d7085
Sneha-Dutt <snehagdutt@gmail.com>
2025-01-25 17:42:20 +0530

    Add bug

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main() {
     int num1 = 5;
     int num2 = 10;
     std::cout << "The sum of " << num1 << " and " << num2 << " is: " << add(num1, num2) << std::endl;
+
+    int array[5];
+    return[6];
     return 0;
 }
